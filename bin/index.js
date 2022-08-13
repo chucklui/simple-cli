262f5e03ded2a74bbb6f48cf6a864f9dcf7870f5
Chuck Lui <luialex0@gmail.com>
2022-08-12 17:50:17 -0700

    style the cli with chalk and boxen

--- bin/index.js
+++ bin/index.js
@@ -1,5 +1,19 @@
 #!/usr/bin/env node
+import chalk from 'chalk';
+import boxen from 'boxen';
 
-console.log(`
-Author: Cheuk Ho Lui
-Phone = 123123123`)
+const name = chalk.white("Name") + ":" + chalk.green('Cheuk Ho Lui');
+const github = chalk.white("GitHub") + ":" + chalk.green('https://github.com/chucklui');
+const boxDimension = {
+  title: 'magical', 
+  titleAlignment: 'center', 
+  padding: 1, 
+  margin: 1, 
+  borderStyle: 'double'
+};
+
+const box = boxen(
+  `${name}\n${github}`, 
+  boxDimension)
+
+console.log(box);
